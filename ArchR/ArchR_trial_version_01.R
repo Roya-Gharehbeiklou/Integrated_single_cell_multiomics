239609efd72d7482c9a25a38c840ae8c82b91a53
Roya-Gharehbeiklou <119226687+Roya-Gharehbeiklou@users.noreply.github.com>
2023-05-22 23:22:18 +0200

    number of threads removed and also arrow files modified

--- ArchR/ArchR_trial_version_01.R
+++ ArchR/ArchR_trial_version_01.R
@@ -9,7 +9,7 @@ library(ArchR, lib.loc="/groups/umcg-franke-scrna/tmp01/projects/multiome/ongoin
 
 set.seed(1)
 addArchRGenome("hg38")
-addArchRThreads(16)
+
 
 
 #Get Input Fragment Files
@@ -17,20 +17,23 @@ inputFiles <- getInputFiles("pbmc_granulocyte_sorted_10k")[1]
 
 names(inputFiles) <- "pbmc_granulocyte_sorted_10k"
 
-# #Create Arrow Files
-# ArrowFiles <- createArrowFiles(
-#   inputFiles = inputFile,
-#   sampleNames = names(inputFile),
-#   minTSS = 4,
-#   minFrags = 1000,
-#   force = TRUE
-# )
+# Create Arrow Files and setting QC values 
+ArrowFiles <- createArrowFiles(
+  inputFiles = inputFile,
+  sampleNames = names(inputFile),
+  minTSS = 4,
+  minFrags = 1000,
+  force = TRUE
+)
 
-# ArrowFiles
-ArrowFiles <-readRDS('/groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/students_hanze_2023/Users/Roya/ArrowFiles/arrowfile.rds')
-ArrowFiles 
+
+ArrowFiles
 
 # Inferring Doublets
+# Had to load IRanges seperately
+library(IRanges, lib.loc='/groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/students_hanze_2023/data/ArchR_libs')
+# Problem with seqnames, had to load it this way
+seqnames <- GenomicRanges::seqnames
 doubScores <- addDoubletScores(
     input = ArrowFiles,
     k = 1, #Refers to how many cells near a "pseudo-doublet" to count.
