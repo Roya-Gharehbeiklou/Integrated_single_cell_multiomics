5de75ed6e5eaa62df87f84206d1e66b029a8cd44
Martijnvanderwerff <martijnvdwerff@hotmail.com>
2023-05-31 13:31:25 +0000

    Peak correlation function working with correct Azimuth annotations

--- logbooks-martijn/FigR_10X_PBMC/FigR-peak-cor.R
+++ logbooks-martijn/FigR_10X_PBMC/FigR-peak-cor.R
@@ -30,20 +30,22 @@ library(ArchR, lib.loc='ArchR_libs')
 library(SummarizedExperiment, lib.loc='Fig_R_libs')
 
 seurat.object <- readRDS('../Users/Karina/pbmc_Seurat_Object_QCfiltered.rds')
-RNAmat <- GetAssayData(object=seurat.object, slot="counts")
+RNAmat <- seurat.object@assays$RNA@data
 dim(RNAmat)
 
 ATAC.data.example <- readRDS('logbooks/FigR/FigR_build_in_data/shareseq_skin_SE_final.rds')
 ATAC.se <- ArchR::import10xFeatureMatrix('output/pbmc_granulocyte_sorted_10k_filtered_feature_bc_matrix.h5', names='')
 ATAC.se
 
+colnames(ATAC.se)<-gsub("#","",colnames(ATAC.se))
+
 ATAC.data <- readRDS('../Users/Roya/Save-ArchR-Project.rds')
 #ATAC.data <- ArchR::addPeakSet(ATAC.data)
 #ATAC.data <- ArchR::addPeakMatrix(ATAC.data)
 
-proj <- addGeneExpressionMatrix(input = ATAC.data, seRNA = ATAC.se, force = TRUE)
+#proj <- addGeneExpressionMatrix(input = ATAC.data, seRNA = ATAC.se, force = TRUE)
 
-annoCols <- readRDS('../Users/Dilya/azimuth_results/pbmc_Seurat_Object_QCfiltered_Azimuth.rds')
+annoCols <- readRDS('../Users/Dilya/azimuth_results/pbmc_Seurat_Azimuth_for_figR.rds')
 #annoCols.correct <- levels(annoCols@active.ident)
 
 ## Get only specific celltypes
@@ -69,31 +71,37 @@ annoCols@meta.data[annoCols@meta.data$predicted.celltype.l2 %in% megakaryocyte, 
 annoCols <- annoCols[, !is.na(annoCols@meta.data$cell_type_lowerres)]
 
 barcodes.atac <- colnames(ATAC.se)
-
+barcodes.rnamat <- colnames(RNAmat)
 # Loosing more barcodes
 annoCols <- annoCols[, rownames(annoCols@meta.data) %in% barcodes.atac]
+annoCols <- annoCols[, rownames(annoCols@meta.data) %in% barcodes.rnamat]
 
 # Keep annotated cells
 cellsToKeep <- sample(colnames(annoCols),replace = FALSE)
-ATAC.se <- ATAC.se[,cellsToKeep]
-RNAmat <- RNAmat[,cellsToKeep]
+ATAC.se <- ATAC.se[,colnames(ATAC.se) %in% cellsToKeep]
+RNAmat <- RNAmat[,colnames(RNAmat) %in% cellsToKeep]
 
 #ATAC.data
 dim(ATAC.se) # Peaks x Cells
-colnames(ATAC.se)<-gsub("#","",colnames(ATAC.se))
+dim(RNAmat)
 
 celltypes.barcode <- data.frame(rownames(annoCols@meta.data), annoCols@meta.data$predicted.celltype.l2)
-celltypes.barcode
+#celltypes.barcode
 
-
-
-RNAmat <- RNAmat[Matrix::rowSums(RNAmat)!=0,]
+ATAC.se@colData <- DataFrame(celltypes.barcode)
 
 library(pbmcapply, lib.loc="R_libs")
+library(BSgenome, lib.loc='Fig_R_libs')
+library(BSgenome.Hsapiens.UCSC.hg38, lib.loc='Fig_R_libs')
 
+# Get correct ATAC.se levels
+levels.atac.data.example <- seqnames(ATAC.data.example)
+
+library(gdata)
+ATAC.se.test <- ATAC.se[startsWith(rowData(ATAC.se)$interval, 'c')]
 
 # Don't run interactively
-cisCorr <- FigR::runGenePeakcorr(ATAC.se = ATAC.se,
+cisCorr <- FigR::runGenePeakcorr(ATAC.se = ATAC.se.test,
                            RNAmat = RNAmat,
                            genome = "hg38", # One of hg19, mm10 or hg38 
                            nCores = 21,
@@ -101,4 +109,14 @@ cisCorr <- FigR::runGenePeakcorr(ATAC.se = ATAC.se,
                            n_bg = 100)
 
 head(cisCorr)
-write.table(cisCorr, '/groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/students_hanze_2023/Users/Martijn/Integrated_single_cell_multiomics/logbooks-martijn/FigR_10X_PBMC/output/', quote=FALSE, sep="\t")
+write.table(cisCorr, '/groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/students_hanze_2023/Users/Martijn/Integrated_single_cell_multiomics/logbooks-martijn/FigR_10X_PBMC/output/ciscor.csv', quote=FALSE, sep="\t")
+
+cisCorr.filt <- cisCorr %>% filter(pvalZ <= 0.05)
+
+dorcGenes <- dorcJPlot(dorcTab = cisCorr.filt,
+                         cutoff = 10, # No. sig peaks needed to be called a DORC
+                         labelTop = 20,
+                         returnGeneList = TRUE, # Set this to FALSE for just the plot
+                         force=2)
+
+ggsave('/groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/students_hanze_2023/Users/Martijn/Integrated_single_cell_multiomics/logbooks-martijn/FigR_10X_PBMC/output/dors.png')
