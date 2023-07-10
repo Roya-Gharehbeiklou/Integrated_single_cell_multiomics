2de6ff57dbeb294deb60d454145bd397ee4133fd
Karina <j.k.diaz.barba@st.hanze.nl>
2023-07-10 19:52:48 +0200

    Frip Values QC added

--- ArchR/ArchR.R
+++ ArchR/ArchR.R
@@ -1,4 +1,7 @@
 #srun --cpus-per-task=1 --mem=10gb --nodes=1 --qos=interactive --time=05:00:00 --pty bash -i
+# Activate conda environment that has the macs2 function
+# cd .conda/envs
+# conda activate mac2_env/
 # module load RPlus
 # R
 # run in /groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/students_hanze_2023/data/ArchR_libs
@@ -18,7 +21,7 @@ library('BSgenome.Hsapiens.UCSC.hg38')
 
 set.seed(1)
 # settting the threads to one, if not there is a problem when inferring the doublets and creating the arrow files. read: https://github.com/GreenleafLab/ArchR/issues/218  for more information
-addArchRThreads(threads = 1)
+#addArchRThreads(threads = 1)
 addArchRGenome("hg38")
 
 #Get Input Fragment Files
@@ -86,32 +89,124 @@ plotPDF(p1,p2, name = "Plot-UMAP-Sample-Clusters.pdf",
 #saveArchRProject(ArchRProj = proj)
 
 # loadArchRProject
-# proj <- loadArchRProject(path ='/groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/students_hanze_2023/Users/Roya')
+#proj <- loadArchRProject(path ='/groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/students_hanze_2023/Users/Roya')
+getAvailableMatrices(proj)
 
-# addGroupCoverage
-proj<-addGroupCoverages(ArchRProj = proj, groupBy = "Clusters")
+# Calling Peaks w/ Macs2. 
+# pathToMacs2 <- findMacs2()
 
- # Saving ArchRProject
-#saveArchRProject(ArchRProj = proj)
-# loadArchRProject
-# proj <- loadArchRProject(path ='/groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/students_hanze_2023/Users/Roya')
-
+# projTmp <- addReproduciblePeakSet(
+#     ArchRProj = proj, 
+#     groupBy = "Clusters", 
+#     pathToMacs2 = pathToMacs2
+# )
+## Error: /Roya/PeakCalls/InsertionBeds/C1._.Rep1-1_summits.bed' does not exist or is non-readable.
 
 # Calling Peaks w/ TileMatrix
-# FRiP (fraction of reads in peaks: 
-#how many of total reads aligned to regions defined as “peaks” after pseudo-bulking) > 0.5
-
 projTmp <- addReproduciblePeakSet(
-    ArchRProj =  proj, 
+    ArchRProj = proj, 
     groupBy = "Clusters",
     peakMethod = "Tiles",
     method = "p"
 )
 
 getPeakSet(projTmp)
+
+# Save
+saveArchRProject(ArchRProj = projTmp)
+# loadArchRProject
+# projTmp<- loadArchRProject(path ='/groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/students_hanze_2023/Users/Roya')
+
 # Adding Peak Matrix
 proj_peak_matrix <- addPeakMatrix(projTmp)
 getAvailableMatrices(proj_peak_matrix)
+
+#Motif Deviations
+if("Motif" %ni% names(proj_peak_matrix@peakAnnotation)){
+    proj_peak_matrix <- addMotifAnnotations(ArchRProj = proj_peak_matrix, motifSet = "cisbp", name = "Motif")
+}
+
+proj_peak_matrix <- addBgdPeaks(proj_peak_matrix)
+
+# Creating a deviations matrix in each of our Arrow files called “MotifMatrix”
+proj_peak_matrix <- addDeviationsMatrix(
+  ArchRProj = proj_peak_matrix, 
+  peakAnnotation = "Motif",
+  force = TRUE
+)
+
+plotVarDev <- getVarDeviations(proj_peak_matrix, name = "MotifMatrix", plot = TRUE)
+plotVarDev
+
+plotPDF(plotVarDev, name = "Variable-Motif-Deviation-Scores", width = 5, height = 5, ArchRProj = proj_peak_matrix, addDOC = FALSE)
+
+
+#Motif Footprinting
+motifPositions <- getPositions(proj_peak_matrix)
+
+motifPositions
+proj_peak_matrix <- addGroupCoverages(ArchRProj = proj_peak_matrix, force=TRUE)
+# seFoot <- getFootprints(
+#   ArchRProj = proj_peak_matrix, 
+#   positions = motifPositions[markerMotifs], 
+#   groupBy = "Clusters"
+# )
+#Error in h(simpleError(msg, call)) : 
+#  error in evaluating the argument 'i' in selecting a method for function '[': object 'markerMotifs' not found
+# plotFootprints(
+#   seFoot = seFoot,
+#   ArchRProj = proj_peak_matrix, 
+#   normMethod = "Subtract",
+#   plotName = "Footprints-Subtract-Bias",
+#   addDOC = FALSE,
+#   smoothWindow = 5
+#)
+
+
+# FRiP (fraction of reads in peaks: 
+#how many of total reads aligned to regions defined as “peaks” after pseudo-bulking) > 0.5
+proj_peak_matrix <- addCoAccessibility(
+    ArchRProj = proj_peak_matrix,
+    reducedDims = "IterativeLSI"
+)
+
+# proj_peak_matrix <- addPeak2GeneLinks(
+#     ArchRProj = proj_peak_matrix,
+#     reducedDims = "IterativeLSI"
+# )
+#Error in addPeak2GeneLinks(ArchRProj = proj_peak_matrix, reducedDims = "IterativeLSI") : 
+#  GeneIntegrationMatrix not in AvailableMatrices
+
+seGroupMotif <- getGroupSE(ArchRProj = proj_peak_matrix, useMatrix = "MotifMatrix", groupBy = "Clusters")
+saveRDS(seGroupMotif, file='seGroupMotif_proj.rds')
+
+# Save
+saveArchRProject(ArchRProj = proj_peak_matrix)
+#load
+#proj_peak_matrix<- loadArchRProject(path ='/groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/students_hanze_2023/Users/Roya')
+
+# Dimension 
+proj_peak_matrix # 10683
+
+# Get a subset of the project based on the Frip value QC
+ids <- which(proj_peak_matrix$FRIP>=0.5) #10051
+cellsPass <- proj_peak_matrix$cellNames[ids]
+proj_subset = proj_peak_matrix[cellsPass, ]
+
+# Saving the project
+saveArchRProject(ArchRProj = proj_subset)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -139,4 +234,13 @@ seGroupMotif <- getGroupSE(ArchRProj = proj_peak_matrix, useMatrix = "MotifMatri
 
 prueba<-readRDS('/groups/umcg-franke-scrna/tmp01/projects/multiome/ongoing/students_hanze_2023/data/logbooks/FigR/FigR_build_in_data/shareseq_skin_SE_final.rds')
 
+### path
 
+
+
+
+
+
+
+prueba@colData
+proj@cellColData
